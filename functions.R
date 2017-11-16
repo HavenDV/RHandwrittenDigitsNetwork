b8a95c0c54f46dd2cdb337089bd45bc00c052b33
Konstantin S <havendv@gmail.com>
2017-11-16 23:41:11 +1000

    Finished part 2 and part 3

--- functions.R
+++ functions.R
@@ -12,3 +12,23 @@ displayData <- function(sel) {
   full_img <- rotate(full_img)
   image(full_img, axes = FALSE, col = grey(seq(0, 1, length = 256))) 
 }
+
+pause <- function() {
+  readline(prompt="Program paused. Press enter to continue.\n")
+}
+
+sigmoid <- function(z) {
+  return(1.0 / (1.0 + exp(-z)))
+}
+
+predict <- function(Theta1, Theta2, X) {
+  m <- nrow(X)
+  extendedX <- cbind(matrix(1, m, 1), X)
+  z2 <- extendedX %*% t(Theta1)
+  a2 <- sigmoid(z2)
+  extendeda2 <- cbind(matrix(1, m, 1), a2)
+  z3 <- extendeda2 %*% t(Theta2)
+  h <- sigmoid(z3)
+  
+  return(matrix(apply(h, 1, which.max)))
+}
--- main.R
+++ main.R
@@ -1,9 +1,8 @@
 ## =========== Used libraries =========== 
-library(utils)
 library(R.matlab)
         
 ## =========== Includes =========== 
-source("displayData.R")
+source("functions.R")
 
 ## =========== Setup the parameters =========== 
 input_layer_size <- 400;  # 20x20 Input Images of Digits
@@ -19,10 +18,20 @@ X <- data[["X"]]
 y <- data[["y"]]
 m <- dim(X)[1]
 
-# Randomly select 100 data points to display
+# Randomly select 100 data points to display and display it
 sel <- X[sample(nrow(X),size = 100),]
-
 displayData(sel);
 
-readline(prompt="Program paused. Press enter to continue.\n")
+## ================ Part 2: Loading Pameters ================
+cat('Loading Saved Neural Network Parameters ...\n')
 
+# Load the weights into variables Theta1 and Theta2
+weights <- readMat('weights.mat')
+Theta1 <- weights[["Theta1"]]
+Theta2 <- weights[["Theta2"]]
+
+## ================= Part 3: Implement Predict =================
+pred <- predict(Theta1, Theta2, X);
+
+compared <- apply(pred,2,`==`,y)
+cat(paste('Training Set Accuracy: \n', mean(as.double(compared)) * 100));
